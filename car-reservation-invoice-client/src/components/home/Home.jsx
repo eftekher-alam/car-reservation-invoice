e704195bf17d36ca10a096a8aba7803cda3ea847
eftekher-alam <eftekher@yahoo.com>
2024-03-29 11:29:29 +0600

    Navbar completed

---
+++ car-reservation-invoice-client/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../nav_bar/Navbar";
+
+const Home = () => {
+    return (
+        <div className="container mx-auto">
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
