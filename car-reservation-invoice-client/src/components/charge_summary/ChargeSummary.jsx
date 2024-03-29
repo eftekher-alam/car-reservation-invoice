33c839c4960eab509169c94a69e8f989ccb685f7
eftekher-alam <eftekher@yahoo.com>
2024-03-29 14:36:59 +0600

    Charges Summary design done

--- car-reservation-invoice-client/src/components/charge_summary/ChargeSummary.jsx
+++ car-reservation-invoice-client/src/components/charge_summary/ChargeSummary.jsx
@@ -1,8 +1,41 @@
+import FormHeader from "../form_header/FormHeader";
 
 const ChargeSummary = () => {
     return (
         <div>
-            ChargeSummary
+            <FormHeader headingText={"Charges Summary"}></FormHeader>
+            <div className="card glass">
+                <div className="card-body text-sm p-4 space-y-4">
+                    <div className="grid grid-cols-12 gap-1 font-semibold relative after:w-full after:h-[1px] after:bg-[#5D5CFF] after:absolute after:-bottom-1 after:left-0">
+                        <div className="col-span-5 xl:col-span-6">Charge</div>
+                        <div className="col-span-1 xl:col-span-2 text-center">Unit</div>
+                        <div className="col-span-3 xl:col-span-2">Rate</div>
+                        <div className="col-span-3 xl:col-span-2">Total</div>
+                    </div>
+                    <div className="grid grid-cols-12 gap-1">
+                        <div className=" col-span-5 xl:col-span-6">Daily</div>
+                        <div className="col-span-1 xl:col-span-2 text-center">1</div>
+                        <div className="col-span-3 xl:col-span-2">$99.00</div>
+                        <div className="col-span-3 xl:col-span-2">$99.00</div>
+                    </div>
+                    <div className="grid grid-cols-12 gap-1">
+                        <div className=" col-span-5 xl:col-span-6">Weekly</div>
+                        <div className="col-span-1 xl:col-span-2 text-center">1</div>
+                        <div className="col-span-3 xl:col-span-2">$390.00</div>
+                        <div className="col-span-3 xl:col-span-2">$390.00</div>
+                    </div>
+                    <div className="grid grid-cols-12 gap-1">
+                        <div className=" col-span-5 xl:col-span-6">Collision Damage Waiver</div>
+                        <div className="col-span-1 xl:col-span-2 text-center"></div>
+                        <div className="col-span-3 xl:col-span-2">$9.00</div>
+                        <div className="col-span-3 xl:col-span-2">$9.00</div>
+                    </div>
+                    <div className="grid grid-cols-12 gap-1 font-semibold">
+                        <div className="col-span-9">Total</div>
+                        <div className="col-span-3">$9.00</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
